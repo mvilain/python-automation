0bc86632dd4a44051336c3a03fcd414570677a20
Michael Vilain <michael@vilain.com>
2022-06-26 12:06:44 -0700

    add print statements to show progress; remove extraneous variables

--- 2.Automate The News/2.news-headless.py
+++ 2.Automate The News/2.news-headless.py
@@ -1,18 +1,29 @@
+#!/usr/bin/which python3
+# 2.news-headless.py
+# extract information from a site using Xpath's chrome webdriver
+# this will NOT open a browser but extract the pages from the URL anyway
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
+import os
+import sys
 
-web = 'https://www.thesun.co.uk/sport/football/'
-path = '/Users/frankandrade/Downloads/chromedriver'  # introduce path here
+WEB = 'https://www.thesun.co.uk/sport/football/'
+PROG = os.path.basename(sys.argv[0])
+DRIVER_PATH = os.path.expanduser('~/Downloads/chromedriver')  # introduce path here
+OUT_FILE = 'headline.csv'
 
+print('{}--> driver.get('.format(PROG), end='', flush=True)
 # add headless mode
 options = Options()
 options.headless = True
-driver_service = Service(executable_path=path)
+driver_service = Service(executable_path=DRIVER_PATH)
 driver = webdriver.Chrome(service=driver_service, options=options)
-driver.get(web)
+driver.get(WEB)
 
+print('{} )'.format(WEB), end='', flush=True)
 containers = driver.find_elements(by='xpath', value='//div[@class="teaser__copy-container"]')
 
 titles = []
@@ -25,9 +36,12 @@ for container in containers:
     titles.append(title)
     subtitles.append(subtitle)
     links.append(link)
+    print(".", end='', flush=True)
 
 my_dict = {'title': titles, 'subtitle': subtitles, 'link': links}
 df_headlines = pd.DataFrame(my_dict)
-df_headlines.to_csv('headline-headless.csv')
+df_headlines.to_csv(OUT_FILE)
+print(" {} [{} entries]".format(OUT_FILE, len(titles)))
 
 driver.quit()
+exit(0)
--- 2.Automate The News/3.news-automation.py
+++ 2.Automate The News/3.news-automation.py
@@ -1,3 +1,9 @@
+#!/usr/bin/which python3
+# 3.news-automation.py
+# extract information from a site using Xpath's chrome webdriver
+# this will NOT open a browser but extract the pages from the URL anyway
+# name output file with date so it can be run via cron regularly
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -6,28 +12,30 @@ from datetime import datetime
 import os
 import sys
 
-# Preparing script before we convert it to executable
-application_path = os.path.dirname(sys.executable)
+WEB = 'https://www.thesun.co.uk/sport/football/'
+PROG = os.path.basename(sys.argv[0])
+DRIVER_PATH = os.path.expanduser('~/Downloads/chromedriver')
 
-# get date in format MMDDYYYY
-now = datetime.now()
-month_day_year = now.strftime("%m%d%Y")
+OUT_FILE = 'football_headlines_{}.csv'.format(datetime.now().strftime("%Y%m%d"))
+## os.path.join ensures name is not OS dependent (e.g. directory seperator MacOS="/", Win="\")
+## but I don't want the file stored where python is stored so commented out
+# OUT_FILE = os.path.join(os.path.dirname(sys.executable), OUT_FILE)
 
-web = 'https://www.thesun.co.uk/sport/football/'
-path = '/Users/frankandrade/Downloads/chromedriver'  # introduce path here
-
+print('{}--> driver.web('.format(PROG), end='', flush=True)
 # Headless mode
 options = Options()
 options.headless = True
-driver_service = Service(executable_path=path)
+driver_service = Service(executable_path=DRIVER_PATH)
 driver = webdriver.Chrome(service=driver_service, options=options)
-driver.get(web)
+driver.get(WEB)
 
+print('{}) '.format(WEB), end='', flush=True)
 containers = driver.find_elements(by='xpath', value='//div[@class="teaser__copy-container"]')
 
 titles = []
 subtitles = []
 links = []
+link_count = 0
 for container in containers:
     title = container.find_element(by='xpath', value='./a/h2').text
     subtitle = container.find_element(by='xpath', value='./a/p').text
@@ -35,12 +43,15 @@ for container in containers:
     titles.append(title)
     subtitles.append(subtitle)
     links.append(link)
+    link_count += 1
+    if (link_count % 5) == 0:  # print "+" when count mod 5 = 0
+        print(".", end='', flush=True)
 
 # Exporting data to the same folder where the executable will be located
 my_dict = {'title': titles, 'subtitle': subtitles, 'link': links}
 df_headlines = pd.DataFrame(my_dict)
-file_name = f'football_headlines_{month_day_year}.csv'
-final_path = os.path.join(application_path, file_name)
-df_headlines.to_csv(final_path)
+df_headlines.to_csv(OUT_FILE)
+print(" {} [{} entries]".format(OUT_FILE, len(titles)))
 
 driver.quit()
+exit(0)
