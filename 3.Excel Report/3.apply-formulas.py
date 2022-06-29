b261d5380263579665541f21196c241c0496bba6
Michael Vilain <michael@vilain.com>
2022-06-29 07:29:05 -0700

    add error checking and enhance comments

--- 3.Excel Report/3.apply-formulas.py
+++ 3.Excel Report/3.apply-formulas.py
@@ -1,22 +1,48 @@
+#!/usr/bin/which python3
+# 3.apply-formulas.py
+# loads the Excel pivot_table.xlsx 'Report' workbook created in 1.make-pivot-table.py
+# select the 'Report' sheet to generate the graph
+# select 3 columns by Gender, Product, Total Sales
+# create a pivot table using Gender for y-axis showing Product and Total Sales
+# output to excel
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
+import os
+import sys
+PROG = os.path.basename(sys.argv[0])
+IN_FILE = os.path.expanduser('barchart.xlsx')
+OUT_FILE = os.path.expanduser('report.xlsx')
 
-wb = load_workbook('barchart.xlsx')
+if not os.path.exists(IN_FILE):
+    print("{} -- file '{}' not found".format(PROG, IN_FILE))
+    exit(1)
+
+# if the barchart.xlsx file has been modified by IntelliJ's ExcelReader,
+# this will throw a 'KeyError' exception
+try:
+    wb = load_workbook(IN_FILE)
+except KeyError:
+    print("{} -- error opening '{}'... regenerate the file".format(PROG, IN_FILE))
+    exit(1)
+
 sheet = wb['Report']
+print("{}--> '{}({})'".format(PROG, IN_FILE, sheet), end='', flush=True)
 
 min_column = wb.active.min_column
 max_column = wb.active.max_column
 min_row = wb.active.min_row
 max_row = wb.active.max_row
 
+# this is what this code does if you did it manually
 # Write an Excel formula with Python
 # sheet['B8'] = '=SUM(B6:B7)'
 # sheet['B8'].style = 'Currency'
 
 # Write multiple formulas with a for loop
-for i in range(min_column+1, max_column+1):  # (B, G+1)
-    letter = get_column_letter(i)
+for i in range(min_column+1, max_column+1):  # (B, G+1) skipping category
+    letter = get_column_letter(i)            # conv column # to letter
     sheet[f'{letter}{max_row + 1}'] = f'=SUM({letter}{min_row + 1}:{letter}{max_row})'
-    sheet[f'{letter}{max_row + 1}'].style = 'Currency'
+    sheet[f'{letter}{max_row + 1}'].style = 'Currency' # formats as "####.## $"
 
-wb.save('report.xlsx')
+wb.save(OUT_FILE)
+print(" --> '{}'".format(OUT_FILE))
--- 3.Excel Report/4.format-cells.py
+++ 3.Excel Report/4.format-cells.py
@@ -1,8 +1,26 @@
+#!/usr/bin/which python3
+# 4.format-cells.py
+# loads the Excel report.xlsx 'Report' workbook created in 2.add-charts.py
+# select the 'Report' sheet
+# annotate cells A1 and A2 with titles
+# output to excel
 from openpyxl import load_workbook
 from openpyxl.styles import Font
+import os
+import sys
+PROG = os.path.basename(sys.argv[0])
+IN_FILE = os.path.expanduser('report.xlsx')
+OUT_FILE = os.path.expanduser('report_january.xlsx')
 
-wb = load_workbook('report.xlsx')
+if not os.path.exists(IN_FILE):
+    print("{} -- file '{}' not found".format(PROG, IN_FILE))
+    exit(1)
+
+# if the barchart.xlsx file has been modified by IntelliJ's ExcelReader,
+# this will throw a 'KeyError' exception
+wb = load_workbook(IN_FILE)
 sheet = wb['Report']
+print("{}--> '{}({})'".format(PROG, IN_FILE, sheet), end='', flush=True)
 
 # Add format
 sheet['A1'] = 'Sales Report'
@@ -10,4 +28,5 @@ sheet['A2'] = 'January'
 sheet['A1'].font = Font('Arial', bold=True, size=20)
 sheet['A2'].font = Font('Arial', bold=True, size=10)
 
-wb.save('report_january.xlsx')
+wb.save(OUT_FILE)
+print(" --> '{}'".format(OUT_FILE))
